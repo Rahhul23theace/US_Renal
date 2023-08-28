9c6ca793f82ac09c01a4dec3a768a7bc854ca38d
Rahhul23theace <128021763+Rahhul23theace@users.noreply.github.com>
2023-08-28 15:57:17 +0800

    Update app_cpu.py

--- app_cpu.py
+++ app_cpu.py
@@ -86,4 +86,4 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
